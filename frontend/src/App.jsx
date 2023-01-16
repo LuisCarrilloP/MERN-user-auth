84e281d21816a6cb218343a0a6a6ec53e5cba647
LuisCarrilloP <jl.carrillopadron@gmail.com>
2023-01-16 13:57:35 -0600

    starting backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route  } from 'react-router-dom';
 
 //Pages
 import Home from './pages/home/Home';
