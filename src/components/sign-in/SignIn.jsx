ccc9b5d922445ede5fef4fbf676ef32c7723b295
LuisCarrilloP <jl.carrillopadron@gmail.com>
2023-01-16 13:10:18 -0600

    input missing style

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -10,14 +10,37 @@ const defaultFormFields = {
 const SignIn = () => {
 
    const [formFields, setFormFields] = useState(defaultFormFields)
+   const [formErrrors, setFormErrrors] = useState({})
 
    const handleInputValueChange = (event) => {
       const { name, value } = event.target
       setFormFields({ ...formFields, [name]: value })
    }
+   const changeBorderColorOnError = (inputName) => {
+      let formInput = document.getElementById(`${inputName}`)
+      formInput.classList.add("error")
+   }
+   const handleValidation = () => {
+      let error = {}
+
+      if(!formFields.email){
+         error.email = "Email is required!"
+         changeBorderColorOnError("email")
+      }
+
+      if(!formFields.password){
+         error.password = "Password is required"
+         changeBorderColorOnError("password")
+      }
+
+      return error
+   }
+
+
    const handleSubmit = (event) => {
       event.preventDefault()
-      console.log(formFields)
+      //console.log(formFields)
+      setFormErrrors(handleValidation)
    }
 
    return (
@@ -28,15 +51,29 @@ const SignIn = () => {
 
             <div className="form-item" id="email">
                <label htmlFor="">Email</label>
-               <input type="text" placeholder='Enter your email' name='email' value={formFields.email}  onChange={handleInputValueChange}/>
+               <input 
+                  type="text" 
+                  placeholder='Enter your email' 
+                  name='email' 
+                  value={formFields.email}  
+                  onChange={handleInputValueChange}
+               />
+               <span className="error-text">{formErrrors.email}</span>
             </div>
 
             <div className="form-item" id="password">
                <label htmlFor="">Password</label>
-               <input type="password" placeholder='Enter your password' name='password' value={formFields.password} onChange={handleInputValueChange}/>
+               <input 
+                  type="password" 
+                  placeholder='Enter your password' 
+                  name='password' 
+                  value={formFields.password} 
+                  onChange={handleInputValueChange}
+               />
+               <span className="error-text">{formErrrors.password}</span>
             </div>
 
-            <button className='form-button' type='submit'>Sign Up</button>
+            <button className='form-button' type='submit'>Sign In</button>
          </form>
 
       </section>
--- src/components/sign-up/SignUp.jsx
+++ src/components/sign-up/SignUp.jsx
@@ -12,13 +12,44 @@ const defaultFormFields = {
 const SignUp = () => {
 
    const [formFields, setFormFields] = useState(defaultFormFields)
+   const [formErrors, setFormErrors] = useState({})
 
    const handleInputValueChange = (event) => {
       const { name, value } = event.target
       setFormFields({ ...formFields, [name]: value })
    }
+   const changeBorderColorOnError = (inputName) => {
+      let formInput = document.getElementById(`${inputName}`)
+      formInput.classList.add("error")
+   }
+   const handleValidation = () => {
+      let error = {}
+
+      if(!formFields.name){
+         error.name = "Name is required!"
+         changeBorderColorOnError("name")
+      }
+
+      if(!formFields.email){
+         error.email = "Email is required!"
+         changeBorderColorOnError("email")
+      }
+
+      if(!formFields.password){
+         error.password = "Password is required"
+         changeBorderColorOnError("password")
+      }
+
+      if(!formFields.confirmPassword){
+         error.confirmPassword = "Confirm your password!"
+         changeBorderColorOnError("confirmPassword")
+      }
+
+      return error
+   }
    const handleSubmit = (event) => {
       event.preventDefault()
+      setFormErrors(handleValidation)
    }
 
    return (
@@ -28,22 +59,50 @@ const SignUp = () => {
          <form onSubmit={handleSubmit}>
             <div className="form-item" id="name">
                <label htmlFor="">Name</label>
-               <input type="text" placeholder='Enter your name' name='name' value={formFields.name} onChange={handleInputValueChange}/>
+               <input 
+                  type="text" 
+                  placeholder='Enter your name' 
+                  name='name' 
+                  value={formFields.name} 
+                  onChange={handleInputValueChange}
+               />
+               <span className="error-text">{formErrors.name}</span>
             </div>
 
             <div className="form-item" id="email">
                <label htmlFor="">Email</label>
-               <input type="text" placeholder='Enter your email' name='email' value={formFields.email}  onChange={handleInputValueChange}/>
+               <input 
+                  type="text" 
+                  placeholder='Enter your email' 
+                  name='email' 
+                  value={formFields.email}  
+                  onChange={handleInputValueChange}
+               />
+               <span className="error-text">{formErrors.email}</span>
             </div>
 
             <div className="form-item" id="password">
                <label htmlFor="">Password</label>
-               <input type="password" placeholder='Enter your password' name='password' value={formFields.password} onChange={handleInputValueChange}/>
+               <input 
+                  type="password" 
+                  placeholder='Enter your password' 
+                  name='password' 
+                  value={formFields.password} 
+                  onChange={handleInputValueChange}
+               />
+               <span className="error-text">{formErrors.password}</span>
             </div>
 
             <div className="form-item" id="confirmPassword">
                <label htmlFor="">Confirm Password</label>
-               <input type="password" placeholder='Enter your password again' name='confirmPassword' value={formFields.confirmPassword} onChange={handleInputValueChange}/>
+               <input 
+                  type="password" 
+                  placeholder='Enter your password again' 
+                  name='confirmPassword' 
+                  value={formFields.confirmPassword} 
+                  onChange={handleInputValueChange}
+               />
+               <span className="error-text">{formErrors.confirmPassword}</span>
             </div>
 
             <button className='form-button' type='submit'>Sign Up</button>
